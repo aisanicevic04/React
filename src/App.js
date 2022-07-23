3be08799a1d556723dcdf6efa247a6ab046a9483
Aisa Nicevic <aisanicevic04@gmail.com>
2022-07-23 18:35:49 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import React from "react";
 import Tabs from "./components/functional/Tabs/Tabs";
 
 const data = [
-    {
-        heading: "Item 1",
-        body: "Hi from item 1"
-    },
-    {
-        heading: "Item 2",
-        body: "Hi from item 2"
-    },
-    {
-        heading: "Item 3",
-        body: "Hi from item 3"
-    }
+  {
+    heading: "Page 1",
+    body: "Hi from page 1",
+  },
+  {
+    heading: "Page 2",
+    body: "Hi from page 2",
+  },
+  {
+    heading: "Page 3",
+    body: "Hi from page 3",
+  },
 ];
 
 export default function App() {
-    return (
-        <div className="App">
-            <Tabs data={data} />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Tabs data={data} />
+    </div>
+  );
 }
